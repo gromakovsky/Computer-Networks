b9f873ee35a3b5467a5c73e7ba56d1536a73acf2
Ivan Gromakovsky <gie1994@gmail.com>
2015-03-27 22:29:21 +0300

    Add very simple and stupid interface

--- Chord-lab/storage.py
+++ Chord-lab/storage.py
@@ -20,13 +20,30 @@ def main():
         sys.stderr.write("Couldn't launch TCP thread, aborting:\n" + str(e))
         sys.exit(1)
 
-    try:
-        while True:
-            pass
-    except KeyboardInterrupt:
-        tcp_listener.socket.close()
-        print('Aborting on keyboard interrupt')
-        sys.exit(0)
+    while True:
+        try:
+            inp = input('Enter command: ')
+            if inp[:3].lower() == 'get':
+                key = input('Enter key: ')
+                value = node.get(key.encode())
+                if value is None:
+                    print('Not found')
+                else:
+                    print('Value:', value)
+            elif inp[:3].lower() == 'put':
+                key = input('Enter key: ')
+                value = input('Enter value: ')
+                if node.put(key.encode(), value.encode()):
+                    print('Success')
+                else:
+                    print('Fail')
+        except KeyboardInterrupt:
+            print('Aborting on keyboard interrupt')
+            udp_listener.socket.close()
+            tcp_listener.socket.close()
+            sys.exit(0)
+        except Exception as e:
+            print('Error occurred:', e)
 
 
 if __name__ == '__main__':
